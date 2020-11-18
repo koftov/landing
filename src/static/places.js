cc238e4a9ebfc66d50b4e7eab27137f9c3d81fdb
koftov <ahronlu@gmail.com>
2020-11-19 01:36:58 +0200

    push to gh

---
+++ src/static/places.js
@@ -0,0 +1,20 @@
+const places = [
+  {
+    title: "Green",
+    description:
+      "The Maldives are home to some of the world's most ravishing islands, but it's the sea. which truly makes these islands shine. Luminous aquamarine waters with a crystal clarity lap upon these dazzling white shores, which barely peek above the Indian Ocean.",
+    imageUrl: process.env.PUBLIC_URL + "/assets/island1.jpg",
+    direction: "left",
+    time: 1500,
+  },
+  {
+    title: "Bora Bora",
+    description:
+      "Shaped like a giant sombrero, this lush volcanic island stars in countless South",
+    imageUrl: process.env.PUBLIC_URL + "/assets/island2.jpg",
+    direction: "right",
+    time: 1500,
+  },
+];
+
+export default places;
